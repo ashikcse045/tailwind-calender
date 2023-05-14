c74b57a11e829cb78a2200352b86dab5b76cc542
ashik <ashikcse321@gmail.com>
2023-05-14 17:57:08 +0600

    nav and table body done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF671F',
+        secondary: '#F9F9F9',
+        dark: '#282A3A',
+      },
+      fontFamily: {
+        bangla: "'Anek Bangla', sans-serif",
+      }
+    },
   },
   plugins: [],
 }
