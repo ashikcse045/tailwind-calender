a9ba46e0225b8ecb5e45b35f3d9d1843ce382a11
ashik <ashikcse321@gmail.com>
2023-05-16 21:04:41 +0600

    fetch events from server

--- dist/script.js
+++ dist/script.js
@@ -44,27 +44,83 @@ async function fetchData() {
   displayMonth.appendChild(textNode);
 
   const currentMonth = months[1];
+  // console.log(currentMonth);
   const dayNum = currentMonth.day;
   const start = currentMonth.start;
+  const krishnaAkadoshi = currentMonth.krishnaAkadoshi;
+  const sukloakadoshi = currentMonth.sukloAkadoshi;
+
+  const omabossa = currentMonth.omabossa;
+  const purnima = currentMonth.purnima;
+
   let index = 0;
   let currentCell = 0;
 
   for (let i = 0; i < 6; i++) {
     const tr = document.createElement("tr");
-    tr.className = 'text-center';
+    tr.className = "text-center";
     for (let j = 0; j < 7; j++) {
       // console.log(j);
       if (currentCell >= start && index < dayNum) {
         const td = document.createElement("td");
-        td.innerHTML = dates[index];
-        td.className = 'w-[14.28%] py-3 border border-gray-200 dark:border-gray-700';
+
+        if (krishnaAkadoshi.includes(index + 1)) {
+          // td.innerHTML = dates[index];
+          const span = document.createElement("span");
+          span.className =
+            "material-symbols-outlined fill-icon text-gray-950 dark:text-gray-400";
+          span.innerText = "clear_night";
+          const p = document.createElement("p");
+          p.className = "text-xs";
+          p.innerText = `${dates[index]} কৃষ্ণ একাদশী`;
+
+          td.append(span, p);
+        } else if (sukloakadoshi.includes(index + 1)) {
+          // td.innerHTML = dates[index];
+          const span = document.createElement("span");
+          span.className =
+            "material-symbols-outlined fill-icon text-yellow-400";
+          span.innerText = "clear_night";
+          const p = document.createElement("p");
+          p.className = "text-xs";
+          p.innerText = `${dates[index]} শুক্ল একাদশী`;
+
+          td.append(span, p);
+        } else if (purnima == index + 1) {
+          // td.innerHTML = dates[index];
+          const span = document.createElement("span");
+          span.className =
+            "material-symbols-outlined fill-icon text-yellow-400";
+          span.innerText = "circle";
+          const p = document.createElement("p");
+          p.className = "text-xs";
+          p.innerText = `${dates[index]} পূর্ণিমা`;
+
+          td.append(span, p);
+        } else if (omabossa == index + 1) {
+          // td.innerHTML = dates[index];
+          const span = document.createElement("span");
+          span.className =
+            "material-symbols-outlined fill-icon text-gray-950 dark:text-gray-400";
+          span.innerText = "circle";
+          const p = document.createElement("p");
+          p.className = "text-xs";
+          p.innerText = `${dates[index]} অমাবস্যা`;
+
+          td.append(span, p);
+        } else {
+          td.innerHTML = dates[index];
+        }
+
+        td.className =
+          "w-[14.28%] py-3 border border-gray-200 dark:border-gray-700";
         tr.appendChild(td);
         index++;
-      }
-      else {
+      } else {
         const td = document.createElement("td");
-        td.innerHTML = '';
-        td.className = 'w-[14.28%] py-3 border border-gray-200 dark:border-gray-700';
+        td.innerHTML = "";
+        td.className =
+          "w-[14.28%] py-3 border border-gray-200 dark:border-gray-700";
         tr.appendChild(td);
         currentCell++;
       }
